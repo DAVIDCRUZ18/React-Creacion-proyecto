c622d1ca69c903e32325533569f977e0cc162a83
David cruz martinez <lx12466@innovaccion.mx>
2023-06-13 20:27:55 -0500

    arreglo de react

--- proyecto/src/components/primercomponente.js
+++ proyecto/src/components/primercomponente.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+
 
 export const Primercomponente = () => {
+  const [Productos, setProductos] = useState([])
+  function Llamar_api() {
+    axios({
+      url:'http://127.0.0.1:4200/Producto/',method:'get'
+    }).then(respuesta => {
+      console.log(respuesta.data);
+      setProductos(respuesta.data)
+    })
+  }
   return (
     <div>
-      <h1>Primer componente</h1>
+      <h1>boton</h1>
       <p>intento #2</p>
+      <button onClick={Llamar_api}>utilizacion de api</button>
+
+      <div>
+        {
+          Productos.length !==0 && 
+          Productos[0].nombre
+        }
+      </div>
     </div>
   )
 }
